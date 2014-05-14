aaf6e99985404de5e77522966490d3603a6d2321
Andrei Pristavu <andrew@andrew.ro>
2014-05-15 01:59:09 +0300

    Update ro_map_jqvmap.js

--- ro_map_jqvmap.js
+++ ro_map_jqvmap.js
@@ -30,7 +30,7 @@
   | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS   |
   | SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE                     |
   +---------------------------------------------------------------------------------+
-    Authors: ANDREI PRISTAVU (andrew (at) andrew.ro            					
+    Authors: ANDREI PRISTAVU andrew (at) andrew.ro            					
   +---------------------------------------------------------------------------------+
 */
 jQuery.fn.vectorMap('addMap', 'ro_map', {
